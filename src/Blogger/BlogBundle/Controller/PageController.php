cffb0ddce55af64ed229b770868b01d6854e7390
cheynede <c.minh.binh@gmail.com>
2015-09-01 22:55:08 +0200

    home and about

--- src/Blogger/BlogBundle/Controller/PageController.php
+++ src/Blogger/BlogBundle/Controller/PageController.php
@@ -3,6 +3,8 @@
 
 namespace Blogger\BlogBundle\Controller;
 
+use Blogger\BlogBundle\Entity\Enquiry;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class PageController extends Controller
@@ -14,5 +16,28 @@ class PageController extends Controller
     public function aboutAction()
     {
         return $this->render('BloggerBlogBundle:Page:about.html.twig');
+    }
+    public function contactAction()
+    {
+        $oEnquiry = new Enquiry();
+        $form = $this->createForm(new EntityType(), $oEnquiry);
+        $request = $this->getRequest();
+        if ($request->getMethod() == 'POST') {
+            $form->bindRequest($request);
+
+            if ($form->isValid()) {
+                // Perform some action, such as sending an email
+
+                // Redirect - This is important to prevent users re-posting
+                // the form if they refresh the page
+                return $this->redirect($this->generateUrl('BloggerBlogBundle_contact'));
+            }
+        }
+
+        return $this->render('BloggerBlogBundle:Page:contact.html.twig', array(
+            'form' => $form->createView()
+        ));
+
+        return $this->render('BloggerBlogBundle:Page:contact.html.twig');
     }
 }
